5e8fc514cad61b697e5acf5f7ebc37ab94418cc3
xiong35 <2242787668@qq.com>
2020-06-21 16:57:19 +0800

    home done

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,45 @@
 // dependency: //
 <template>
   <v-app>
-    <nuxt />
+    <v-app-bar absolute>
+      <v-avatar size="30" class="mr-3 d-md-block d-none">
+        <img :src="require('~/static/img/avatar.png')" alt="avatar" />
+      </v-avatar>
+      <v-toolbar-title class="font-weight-black"
+        >XRZ's Home</v-toolbar-title
+      >
+      <div class="mx-auto"></div>
+    </v-app-bar>
+
+    <v-main>
+      <v-row class="pt-12 ma-3">
+        <v-col class="col-3 pa-4 mt-12">
+          <v-tabs
+            v-model="tab"
+            background-color="transparent"
+            color="blue-grey darken-3"
+            vertical
+          >
+            <v-tab to="/"><v-icon left>mdi-home</v-icon> 主页</v-tab>
+            <v-tab to="web_study"
+              ><v-icon left>mdi-web</v-icon> 网络学习</v-tab
+            >
+            <v-tab to="basketball"
+              ><v-icon left>mdi-basketball</v-icon> 篮球!</v-tab
+            >
+            <v-tab @click="$router.go(-1)">
+              <v-icon left>
+                mdi-chevron-left-circle-outline
+              </v-icon>
+              后退
+            </v-tab>
+          </v-tabs>
+        </v-col>
+        <v-col class="col-9 pa-4 mt-7">
+          <nuxt />
+        </v-col>
+      </v-row>
+    </v-main>
   </v-app>
 </template>
 
@@ -10,7 +48,9 @@
     name: "default",
     components: {},
     data() {
-      return {};
+      return {
+        tab: 0,
+      };
     },
     computed: {},
     watch: {},
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,93 @@
-// dependency: 
-// 
 <template>
   <div class="index">
-    <nuxt-link to="web_study">web_study</nuxt-link>
-    <nuxt-link to="about">about</nuxt-link>
+    <v-card class="mx-auto" hover max-width="800" outlined>
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="subtitle-2 mb-4">个人简介</div>
+          <v-list-item-title class="headline mb-1">
+            徐睿泽
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            机械科学与工程学院 1910
+          </v-list-item-subtitle>
+          <span class="mt-3">
+            身高185, 体重185 <br />
+            表面上胸无大志混吃等死, 实际上乐观开朗无忧无虑
+          </span>
+        </v-list-item-content>
+
+        <v-list-item-avatar
+          tile
+          size="120"
+          color="grey"
+        ></v-list-item-avatar>
+      </v-list-item>
+
+      <v-card-actions class="px-4">
+        联系我:
+        <v-btn text class="ml-2" color="black" @click="copy('q')">
+          <v-icon left>mdi-qqchat</v-icon>
+          qq
+        </v-btn>
+        <v-btn text color="black" @click="copy('v')">
+          <v-icon left>mdi-wechat</v-icon>
+          微信
+        </v-btn>
+        <v-snackbar
+          top
+          v-model="snackBarQ"
+          color="success"
+          :timeout="1400"
+        >
+          已复制QQ号到剪切板😉
+        </v-snackbar>
+        <v-snackbar
+          top
+          v-model="snackBarV"
+          color="success"
+          :timeout="1400"
+        >
+          已复制微信号到剪切板😉
+        </v-snackbar>
+      </v-card-actions>
+    </v-card>
+
+    <v-card max-width="800" hover outlined class="mx-auto">
+      <v-subheader>学习分享</v-subheader>
+      <p class="mx-4 headline">
+        网络学习
+      </p>
+
+      <v-card-text>
+        学海无涯, <del>回头是岸</del>要想学好, 除了要勤奋, 还得有好的学习方法! <br>
+        来看看我总结的学习经验吧!
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn nuxt to="web_study" text color="deep-purple accent-4">
+          了解更多
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <v-card max-width="800" hover outlined class="mx-auto">
+      <v-subheader>兴趣爱好</v-subheader>
+      <p class="mx-4 headline">
+        篮球🏀
+      </p>
+
+      <v-img :src="require('~/static/img/5.jpg')" height="410"></v-img>
+
+      <v-card-text>
+        你见过凌晨四点的洛杉矶吗?
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn nuxt to="basketball" text color="deep-purple accent-4">
+          了解更多
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
@@ -12,14 +96,43 @@
     name: "index",
     components: {},
     data() {
-      return {};
+      return {
+        snackBarQ: false,
+        snackBarV: false,
+      };
     },
     computed: {},
     watch: {},
-    methods: {},
+    methods: {
+      copy(type) {
+        let number = "";
+        if (type === "q") {
+          this.snackBarQ = true;
+          number = "404675430";
+        } else if (type === "v") {
+          this.snackBarV = true;
+          number = "xrz666404675430";
+        }
+        let textArea = document.createElement("textarea");
+        textArea.style.zIndex = -1;
+        textArea.value = number;
+        document.body.appendChild(textArea);
+        textArea.select();
+        try {
+          document.execCommand("copy");
+        } catch (err) {
+          alert("该浏览器不支持点击复制到剪贴板");
+        }
+        document.body.removeChild(textArea);
+      },
+    },
     created() {},
     mounted() {},
   };
 </script>
 <style scoped>
+  .v-card {
+    cursor: default;
+    margin: 20px 0 20px 0;
+  }
 </style>
